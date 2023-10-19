f9166da9c4bede7e7ecb8d080fd38cc2cb928d8e
Selim <selim.mellouk@essec.edu>
2023-10-19 16:38:50 +0200

    added sqlite tp10 exo2

--- tps/tp_10_exo_2_correction.py
+++ tps/tp_10_exo_2_correction.py
@@ -1,6 +1,5 @@
 from urllib import request
 from urllib.parse import quote
-
 import ssl
 import pandas as pd
 
@@ -22,20 +21,55 @@ print(len(l_df), "tables récupérées et parsées")
 chateaux_royaux = l_df[0]
 chateaux_nobiliaires = l_df[1]
 
+
+######################################################
+#  Insertion to PostgreSQL de scalingo
+######################################################
+
 URL_DB = "postgres://course_pyth_5801:Z1cufoxTonxlgtnmHFNb@course-pyth-5801.postgresql.a.osc-fr1.scalingo-dbs.com:32624/course_pyth_5801?sslmode=prefer"
 URL_DB_SQLACHEMY = f"postgresql+psycopg2{URL_DB[8:]}"
-
 engine = create_engine(URL_DB_SQLACHEMY, connect_args={"sslmode": "allow"})
+
+# chateaux_royaux.to_sql(
+#     "chateaux_royaux",
+#     con=engine,
+#     if_exists="replace",
+#     index=True,
+#     chunksize=None,
+# )
+# chateaux_nobiliaires.to_sql(
+#     "chateaux_nobiliaires",
+#     con=engine,
+#     if_exists="replace",
+# )
+
+
+engine.dispose()
+
+
+######################################################
+#  Insertion into sqlite
+######################################################
+import sqlite3
+
+conn = sqlite3.connect("data/tp_10_exo_1.db")
 
 chateaux_royaux.to_sql(
     "chateaux_royaux",
-    con=engine,
+    con=conn,
     if_exists="replace",
     index=True,
     chunksize=None,
 )
 chateaux_nobiliaires.to_sql(
     "chateaux_nobiliaires",
-    con=engine,
+    con=conn,
     if_exists="replace",
 )
+
+chateaux_nobiliaires_verif = pd.read_sql(
+    "SELECT * FROM chateaux_nobiliaires;", con=conn
+)
+
+
+conn.close()
